6a34ca076b46106bba68a92872c83ead202eefd6
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2020-10-15 19:31:29 +0200

    Update reader.go

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -19,3 +19,16 @@ func main() {
 // Implement a type that satisfies the io.Reader interface and reads from another io.Reader,
 // modifying the stream by removing the spaces.
 // Expected output: "Helloworld!"
+
+func (reader spaceEraser) Read(p []byte) (int, error) { //parcours list for tha amount of chara read
+	n, err := reader.r.Read(p)
+	charact := 0 //counter for character != space
+	for i := 0; i<n; i++{
+		if p[i] != 32{ //test is charact is a space (32 = ASCII space)
+			p[charact] = p[i]
+			charact++
+		}
+	}
+	return charact, err
+}
+
