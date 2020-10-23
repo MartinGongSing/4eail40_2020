b05bfda3cf8df5f308004558ed8ee07301d6567b
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2020-10-23 15:01:45 +0200

    tes2

--- exercises/chess/model/coord/cartesian_test.go
+++ exercises/chess/model/coord/cartesian_test.go
@@ -3,5 +3,11 @@ package coord
 import "testing"
 
 func TestNewCartesian(t *testing.T){
-	t.Error("test failed")
+	//t.Error("test failed")
+	c := NewCartesian(1, 2)
+	if (c.x == 1) && (c.y == 2) {
+		// test succes
+	} else {
+		t.Errorf("expected 1 and 2 as coordinates")
+	}
 }
