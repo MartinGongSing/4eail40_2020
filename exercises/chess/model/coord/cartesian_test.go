35199a735f97ed1a94fe7db342850774d0c71e67
MartinGongSing <47624744+MartinGongSing@users.noreply.github.com>
2020-10-30 12:59:48 +0100

    TestCartesian_Coord

--- exercises/chess/model/coord/cartesian_test.go
+++ exercises/chess/model/coord/cartesian_test.go
@@ -11,3 +11,33 @@ func TestNewCartesian(t *testing.T){
 		t.Errorf("expected 1 and 2 as coordinates")
 	}
 }
+
+
+func TestCartesian_Coord(t *testing.T) {
+	c := NewCartesian(1, 2)
+
+	tests := map[int]int{
+		0: 1,
+		1: 2,
+	}
+
+	for n, want := range tests {
+		t.Run(string(rune(n)), func(t *testing.T) {
+			got, err := c.Coord(n)
+			if err != nil {
+				t.Error(err)
+			}
+			if got != want {
+				t.Errorf("expected %d, but got %d", want, got)
+			}
+		})
+	}
+
+	// test for err
+	t.Run("err", func(t *testing.T) {
+		_, err := c.Coord(2)
+		if err == nil {
+			t.Errorf("expected and error for n == 2")
+		}
+	})
+}
